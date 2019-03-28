6650f714036ba161e65913fdc479af6326a06d26
MDadash <dadash.rita@gmail.com>
2019-03-28 16:26:10 +0200

    cat.js #2

--- wp-content/themes/mainTheme/js/category.js
+++ wp-content/themes/mainTheme/js/category.js
@@ -9,13 +9,5 @@
             }
         });
 
-        $.ajax({
-            type: "GET",
-            url: "/wp-admin/admin-ajax.php?action=getimagesbyflat&flat_id=2460304",
-            success: function(response){
-                console.log(JSON.parse(response));
-            }
-        });
-
     });
 }());
