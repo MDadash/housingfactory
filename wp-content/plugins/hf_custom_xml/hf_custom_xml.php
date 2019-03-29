ebbbbbecee8553a257b407a543643c211d9d5b45
DimaZholtikov <dimazholtikov690@gmail.com>
2019-03-29 10:32:20 +0200

    add_new_flats_content

--- wp-content/plugins/hf_custom_xml/hf_custom_xml.php
+++ wp-content/plugins/hf_custom_xml/hf_custom_xml.php
@@ -12,7 +12,7 @@ function getAllXml() {
     $file = XML_FILE_DIR . '/hfxml.xml';
     $xml = simplexml_load_file($file, null, LIBXML_NOCDATA);
     $xmlJson = json_encode($xml);
-    $xmlArray = json_decode($xmlJson,TRUE);
+    $xmlArray = json_decode($xmlJson,true);
     return $xmlArray['Ad'];
 }
 
@@ -50,4 +50,49 @@ function getImagesByFlatId() {
     }
     echo json_encode($imagesByFlatId);
     wp_die();
+}
+
+//function setNewFlats(): void {
+//    $file1 = XML_FILE_DIR . 'hfxml.xml';
+//    $xml1 = simplexml_load_file($file1, null, LIBXML_NOCDATA);
+//    $xmlJson1 = json_encode($xml1);
+//    $xmlArray1 = json_decode($xmlJson1,true)['Ad'];
+//
+//    $file2 = XML_FILE_DIR . 'hfxml2.xml';
+//    $xml2 = simplexml_load_file($file2, null, LIBXML_NOCDATA);
+//    $xmlJson2 = json_encode($xml2);
+//    $xmlArray2 = json_decode($xmlJson2,true)['Ad'];
+//
+//    foreach ($xmlArray1 as $val1) {
+//        foreach ($xmlArray2 as $key2 => $val2) {
+//            if ($val1['Id'] === $val2['Id']) {
+//                unset($xmlArray2[$key2]);
+//            }
+//        }
+//    }
+//
+//    if ($xmlArray2) {
+//        writeNewFlatsToFile($xmlArray2);
+//    }
+//}
+//
+//function writeNewFlatsToFile($data): void {
+//    $fd = fopen(XML_FILE_DIR . 'new_flats.txt', 'w');
+//    $str = json_encode($data);
+//    fwrite($fd, $str);
+//    fclose($fd);
+//}
+//
+//function getNewFlats() {
+//    $str = file_get_contents(XML_FILE_DIR . 'new_flats.txt');
+//    return json_decode($str, true);
+//}
+
+function getLasTwelveFlats() {
+    $file = XML_FILE_DIR . '/hfxml.xml';
+    $xml = simplexml_load_file($file, null, LIBXML_NOCDATA);
+    $xmlJson = json_encode($xml);
+    $xmlArray = json_decode($xmlJson,true)['Ad'];
+    $result = array_slice($xmlArray, -12, 12);
+    return array_reverse($result);
 }
