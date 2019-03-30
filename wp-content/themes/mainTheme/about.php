9347bb4f68f197fa7c4b670add82e9d7e6e9c86e
MDadash <dadash.rita@gmail.com>
2019-03-30 12:05:35 +0200

    Filled 'about us' page - added wp slider added ability to change text on the main page database

--- wp-content/themes/mainTheme/about.php
+++ wp-content/themes/mainTheme/about.php
@@ -10,10 +10,22 @@ Template Name: about
   <section class="container container--articles">
     <div class="row">
       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-          <h2 class="col-12"><?php the_title(); ?></h2>
-          <div class="col-12"><?php the_content(); ?></div>
+          <h2 class="col-12 mb-3"><?php the_title(); ?></h2>
+
+          <div class="col-12 about_content"><?php the_content(); ?></div>
+
+          <div class="col-12 my-3 about_photo">
+              <?php echo do_shortcode('[slick-slider category="47" design="design-1"]'); ?>
+          </div>
+
+        <div class="col-12 my-3">
+            <?php echo do_shortcode('[slick-carousel-slider category="48" autoplay="false" design="design-6" centermode="true" slidestoshow="2"]'); ?>
+        </div>
       <?php endwhile; endif; ?>
     </div>
+
+
+
 	</section>
 </main>
 
